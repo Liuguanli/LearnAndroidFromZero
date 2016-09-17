181abe91786c057bd3be7548f53921cee3e0c6fe
liuguanli <liuguanli1205@qq.com>
2016-09-18 07:55:46 +0800

    add listview

--- app/src/main/java/com/hi/baidu/learnandroidfromzero/MainActivity.java
+++ app/src/main/java/com/hi/baidu/learnandroidfromzero/MainActivity.java
@@ -1,15 +1,24 @@
 package com.hi.baidu.learnandroidfromzero;
 
+import java.util.Arrays;
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private MainAdapter adapter;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        List<String> list =
+                Arrays.asList(new String[] {"四大组件", "网络", "动画", "常用控件", "日志", "从零搭建Android", "学习Android底层", "单测",
+                });
+
+        adapter = new MainAdapter<String>(list, this);
+        mListView = (ListView) findViewById(R.id.listview);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(this);
     }
 
     @Override
@@ -49,4 +66,10 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(MainActivity.this, (String) adapter.getDateResc().get(i), Toast.LENGTH_SHORT).show();
+    }
+    
 }
---
+++ app/src/main/java/com/hi/baidu/learnandroidfromzero/MainAdapter.java
@@ -0,0 +1,95 @@
+package com.hi.baidu.learnandroidfromzero;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by liuguanli on 16/9/17.
+ */
+public class MainAdapter<T> extends BaseAdapter {
+
+    private List<T> mList;
+    private LayoutInflater mInflater = null;
+
+    public List<T> getDateResc() {
+        if (mList == null) {
+            return new ArrayList<>();
+        }
+        return mList;
+    }
+
+    public MainAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
+
+    public MainAdapter(List<T> list, Context context) {
+        mList = list;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    public void changeDate(List<T> list) {
+        if (mList != null) {
+            mList.clear();
+            mList.addAll(list);
+        } else {
+            mList = list;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        if (mList != null) {
+            return mList.size();
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public Object getItem(int i) {
+        if (mList != null) {
+            return mList.get(i);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Get the row id associated with the specified position in the list.
+     *
+     * @param i
+     *
+     * @return
+     */
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder = null;
+        if (view == null) {
+            view = mInflater.inflate(R.layout.listview_item, null);
+            viewHolder = new ViewHolder();
+            viewHolder.tv = (TextView) view.findViewById(R.id.tv);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        String s = (String) mList.get(i);
+        viewHolder.tv.setText(s);
+        return view;
+    }
+
+    private static class ViewHolder {
+        TextView tv;
+    }
+}
