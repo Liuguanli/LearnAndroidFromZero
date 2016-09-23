d5efdc5af35743609518efaa8302d882e3df571b
liuguanli <liuguanli1205@qq.com>
2016-09-23 10:24:52 +0800

    add swipeRefresh

--- app/src/main/java/com/hi/baidu/learnandroidfromzero/activity/RecycleViewActivity.java
+++ app/src/main/java/com/hi/baidu/learnandroidfromzero/activity/RecycleViewActivity.java
@@ -9,17 +9,21 @@ import com.hi.baidu.learnandroidfromzero.adapter.RecycleViewAdapter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class RecycleViewActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private RecyclerView.Adapter adapter;
+
+    private SwipeRefreshLayout mSwipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +32,27 @@ public class RecycleViewActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+
+        mSwipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(RecycleViewActivity.this, "dfdf", Toast.LENGTH_SHORT).show();
+                mSwipeLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSwipeLayout.setRefreshing(false);
+                    }
+                },2000);
+            }
+        });
+
+
+
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+
+
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
---
+++ app/src/main/java/com/hi/baidu/learnandroidfromzero/activity/SwipeRefreshActivity.java
@@ -0,0 +1,31 @@
+package com.hi.baidu.learnandroidfromzero.activity;
+
+import com.hi.baidu.learnandroidfromzero.R;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SwipeRefreshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_refresh);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
